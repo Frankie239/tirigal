72a61f09b046f34608950ebdbe24463481667733
Fran Gimenez <frangimenez98@gmail.com>
2023-04-01 12:26:22 -0300

    Add the first practice

---
+++ ejercicio1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // declarar las variables
+    float primerValor = 0;
+    float segundoValor = 0;
+    float resultado = 0;
+    
+    // pedimos el ingreso de los datos.
+    cout << "Por favor ingrese el primer valor: " << endl;
+    cin >> primerValor;
+    cout << "Por favor ingrese el segundo valor: " << endl;
+    cin >> segundoValor;
+
+    // sumamos los dos datos
+    resultado = primerValor + segundoValor;
+
+    // dividir por la cantidad de numeros. (/2)
+    resultado /= 2;
+
+    // mostrar el promedio
+    cout << "El resultado es: "<< resultado << endl;
+    return 0;
+}
