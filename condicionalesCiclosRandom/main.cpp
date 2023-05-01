13959894f97d60c13b89e21f90ec9ed06536a9f7
Fran Gimenez <frangimenez98@gmail.com>
2023-05-01 12:47:20 -0300

    Add the excersise n3 to the file.

--- condicionalesCiclosRandom/main.cpp
+++ condicionalesCiclosRandom/main.cpp
@@ -74,6 +74,52 @@ int main()
         cout << x << endl;
     }
 #pragma endregion
+#pragma region Ejercicio 3
+    // Le doy un numero a la maquina
+    // La maquina genera un numero aleatorio
+    // si el numero aleatorio es mas grande, hago que los siguientes aleatorios sean mas chicos que el numero generado,
+    srand(time(0));
+
+    // Valores aleatorios entre 1 y 100 usando el módulo:
+    int numeroSecreto = 0;
+
+    int intentos = 0;
+    int valor;
+    int valorMinimo = 1;
+    int valorMaximo = 100;
+    int valorAdivinado;
+
+    cout << "\tBienvenido a Adivina el Número\n\n";
+    while (numeroSecreto < 1 && numeroSecreto > 100)
+    {
+        cout << "Ingrese un numero para que la maquina adivine" << endl;
+        cin >> numeroSecreto;
+    }
+
+    do
+    {
+        valorAdivinado = rand() % (valorMaximo - valorMinimo + 1) + valorMinimo;
+        intentos++;
+
+        if (valorAdivinado > numeroSecreto)
+        {
+            cout << "el varlor" << valorAdivinado << "Demasiado alto!\n\n";
+            valorMaximo = valorAdivinado - 1;
+        }
+        else if (valorAdivinado < numeroSecreto)
+        {
+            cout << "el varlor" << valorAdivinado << "Demasiado bajo!\n\n";
+            valorMinimo = valorAdivinado + 1;
+        }
+        else
+        {
+            cout << "La máquina adivino el número (" << valorAdivinado << ") después de " << intentos << " intentos!\n";
+        }
+
+    } while (valorAdivinado != numeroSecreto);
+
+    cout << endl;
+#pragma endregion
 #pragma region Ejercicio 4
     x = 0;
     int y = 0;
